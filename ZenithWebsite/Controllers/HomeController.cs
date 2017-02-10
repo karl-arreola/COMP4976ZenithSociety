5ae421cb058365865ecc9b49da97456e15aa1c1d
karl-arreola <karreola@my.bcit.ca>
2017-02-09 17:07:44 -0800

    basic home layout

--- ZenithWebsite/Controllers/HomeController.cs
+++ ZenithWebsite/Controllers/HomeController.cs
@@ -3,27 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ZenithDataLib.Models;
 
 namespace ZenithWebsite.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
+            //var events = db.Event.Include(a => a.Activity);
             return View();
         }
     }
