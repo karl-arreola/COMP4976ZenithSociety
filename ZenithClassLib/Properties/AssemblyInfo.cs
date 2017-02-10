e7a96fd399b0ee0233bd1018fecf3e8183f97d99
Matthew Rosenlund <mrosenlund@my.bcit.ca>
2017-02-09 20:17:19 -0800

    Project ZenithClassLib renamed to ZenithDataLib and changed all references to it.

--- ZenithClassLib/Properties/AssemblyInfo.cs
+++ ZenithClassLib/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ZenithClassLib")]
+[assembly: AssemblyTitle("ZenithDataLib")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ZenithClassLib")]
+[assembly: AssemblyProduct("ZenithDataLib")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
