c0b1304fb7132370ddd15dad60b6b16c19ae9df7
Matthew Rosenlund <mrosenlund@my.bcit.ca>
2017-02-09 22:04:32 -0800

    Registration updated to take Username, email, firstname, lastname. ManageController index view displays logged in First name and last name

---
+++ ZenithWebsite/Migrations/ZenithMigrations/201702100517399_UserNameUpdate.cs
@@ -0,0 +1,16 @@
+namespace ZenithWebsite.Migrations.ZenithMigrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UserNameUpdate : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
