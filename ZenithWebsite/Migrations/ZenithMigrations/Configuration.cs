5351f48708a97a87971b31ea6b48dc67ce1b77a3
karl-arreola <karreola@my.bcit.ca>
2017-02-12 23:37:22 -0800

    added more seed data for current, previous, and future week

--- ZenithWebsite/Migrations/ZenithMigrations/Configuration.cs
+++ ZenithWebsite/Migrations/ZenithMigrations/Configuration.cs
@@ -82,7 +82,13 @@ namespace ZenithWebsite.Migrations.ZenithMigrations
                 new Activity {ActivityDescription = "Snowboard Tournament", CreationDate = new DateTime(2017, 2, 10)},
                 new Activity {ActivityDescription = "Biking Race", CreationDate = new DateTime(2017, 2, 11)},
                 new Activity {ActivityDescription = "Basketball Game", CreationDate = new DateTime(2017, 2, 11)},
-                new Activity {ActivityDescription = "Soccer Game", CreationDate = new DateTime(2017, 2, 11)}
+                new Activity {ActivityDescription = "Soccer Game", CreationDate = new DateTime(2017, 2, 11)},
+                new Activity {ActivityDescription = "Golf Tournament", CreationDate = new DateTime(2017, 2, 12)},
+                new Activity {ActivityDescription = "Swimming Race", CreationDate = new DateTime(2017, 2, 12)},
+                new Activity {ActivityDescription = "Doubles Tennis Game", CreationDate = new DateTime(2017, 2, 12)},
+                new Activity {ActivityDescription = "Family Bowling", CreationDate = new DateTime(2017, 2, 13)},
+                new Activity {ActivityDescription = "Rock Concert", CreationDate = new DateTime(2017, 2, 13)},
+                new Activity {ActivityDescription = "Potluck", CreationDate = new DateTime(2017, 2, 13)}
             };
 
             return activities.ToArray();
@@ -93,7 +99,8 @@ namespace ZenithWebsite.Migrations.ZenithMigrations
             var events = new List<Event>()
             {
                 new Event
-                { DateFrom = new DateTime(2017, 2, 10, 9, 0, 0),
+                {
+                    DateFrom = new DateTime(2017, 2, 11, 9, 0, 0),
                     DateTo = new DateTime(2017, 2, 11, 12, 0, 0),
                     EventMadeBy = "a",
                     IsActive = true,
@@ -101,55 +108,62 @@ namespace ZenithWebsite.Migrations.ZenithMigrations
                     CreationDate = new DateTime(2017, 2, 9, 0, 0, 1)
                 },
                 new Event
-                { DateFrom = new DateTime(2017, 2, 10, 14, 0, 0),
-                    DateTo = new DateTime(2017, 2, 11, 17, 0, 0),
+                {
+                    DateFrom = new DateTime(2017, 2, 12, 14, 0, 0),
+                    DateTo = new DateTime(2017, 2, 12, 17, 0, 0),
                     EventMadeBy = "a",
                     IsActive = true,
                     ActivityId = context.Activity.FirstOrDefault(c => c.ActivityDescription ==  "Triathalon").ActivityId,
                     CreationDate = new DateTime(2017, 2, 9, 0, 0, 2)
                 },
                 new Event
-                { DateFrom = new DateTime(2017, 2, 11, 9, 0, 0),
-                    DateTo = new DateTime(2017, 2, 11, 12, 0, 0),
+                {
+                    DateFrom = new DateTime(2017, 2, 14, 9, 0, 0),
+                    DateTo = new DateTime(2017, 2, 14, 12, 0, 0),
                     EventMadeBy = "a",
                     IsActive = true,
                     ActivityId = context.Activity.FirstOrDefault(c => c.ActivityDescription ==  "Biking Race").ActivityId,
                     CreationDate = new DateTime(2017, 2, 9, 0, 0, 3)
                 },
                 new Event
-                { DateFrom = new DateTime(2017, 2, 11, 14, 0, 0),
-                    DateTo = new DateTime(2017, 2, 11, 17, 0, 0),
+                {
+                    DateFrom = new DateTime(2017, 2, 14, 14, 0, 0),
+                    DateTo = new DateTime(2017, 2, 14, 17, 0, 0),
                     EventMadeBy = "Jim",
                     IsActive = true,
                     ActivityId = context.Activity.FirstOrDefault(c => c.ActivityDescription ==  "Basketball Game").ActivityId,
                     CreationDate = new DateTime(2017, 2, 10, 0, 0, 4)
                 },
                 new Event
-                { DateFrom = new DateTime(2017, 2, 11, 9, 0, 0),
-                    DateTo = new DateTime(2017, 2, 11, 12, 0, 0),
+                {
+                    DateFrom = new DateTime(2017, 2, 16, 9, 0, 0),
+                    DateTo = new DateTime(2017, 2, 16, 12, 0, 0),
                     EventMadeBy = "Sara",
                     IsActive = true,
                     ActivityId = context.Activity.FirstOrDefault(c => c.ActivityDescription ==  "Soccer Game").ActivityId,
                     CreationDate = new DateTime(2017, 2, 10, 0, 0, 5)
                 },
                 new Event
-                { DateFrom = new DateTime(2017, 2, 12, 14, 0, 0),
-                    DateTo = new DateTime(2017, 2, 12, 17, 0, 0),
+                {
+                    DateFrom = new DateTime(2017, 2, 16, 14, 0, 0),
+                    DateTo = new DateTime(2017, 2, 16, 17, 0, 0),
                     EventMadeBy = "a",
                     IsActive = true,
                     ActivityId = context.Activity.FirstOrDefault(c => c.ActivityDescription ==  "Triathalon").ActivityId,
                     CreationDate = new DateTime(2017, 2, 11, 0, 0, 6)
                 },
                 new Event
-                { DateFrom = new DateTime(2017, 2, 13, 9, 0, 0),
-                    DateTo = new DateTime(2017, 2, 13, 12, 0, 0),
+                {
+                    DateFrom = new DateTime(2017, 2, 17, 9, 0, 0),
+                    DateTo = new DateTime(2017, 2, 17, 12, 0, 0),
                     EventMadeBy = "Sally",
-                    IsActive = false,
+                    IsActive = true,
                     ActivityId = context.Activity.FirstOrDefault(c => c.ActivityDescription ==  "Snowboard Tournament").ActivityId,
                     CreationDate = new DateTime(2017, 2, 11, 0, 0, 7)
                 },
                 new Event
-                { DateFrom = new DateTime(2017, 2, 13, 14, 0, 0),
+                {
+                    DateFrom = new DateTime(2017, 2, 13, 14, 0, 0),
                     DateTo = new DateTime(2017, 2, 13, 17, 0, 0),
                     EventMadeBy = "Jordan",
                     IsActive = false,
@@ -157,20 +171,76 @@ namespace ZenithWebsite.Migrations.ZenithMigrations
                     CreationDate = new DateTime(2017, 2, 11, 0, 0, 8)
                 },
                 new Event
-                { DateFrom = new DateTime(2017, 2, 14, 9, 0, 0),
-                    DateTo = new DateTime(2017, 2, 14, 10, 0, 0),
+                {
+                    DateFrom = new DateTime(2017, 2, 19, 9, 0, 0),
+                    DateTo = new DateTime(2017, 2, 19, 10, 0, 0),
                     EventMadeBy = "Sally",
-                    IsActive = false,
+                    IsActive = true,
                     ActivityId = context.Activity.FirstOrDefault(c => c.ActivityDescription ==  "Basketball Game").ActivityId,
                     CreationDate = new DateTime(2017, 2, 12, 0, 0, 9)
                 },
                 new Event
-                { DateFrom = new DateTime(2017, 2, 15, 14, 0, 0),
+                {
+                    DateFrom = new DateTime(2017, 2, 15, 14, 0, 0),
                     DateTo = new DateTime(2017, 2, 15, 16, 0, 0),
                     EventMadeBy = "Jordan",
-                    IsActive = false,
+                    IsActive = true,
                     ActivityId = context.Activity.FirstOrDefault(c => c.ActivityDescription ==  "Soccer Game").ActivityId,
                     CreationDate = new DateTime(2017, 2, 12, 0, 0, 10)
+                },
+                new Event
+                {
+                    DateFrom = new DateTime(2017, 2, 20, 17, 0, 0),
+                    DateTo = new DateTime(2017, 2, 20, 20, 0, 0),
+                    EventMadeBy = "Jen",
+                    IsActive = true,
+                    ActivityId = context.Activity.FirstOrDefault(c => c.ActivityDescription ==  "Potluck").ActivityId,
+                    CreationDate = new DateTime(2017, 2, 12, 0, 0, 11)
+                },
+                new Event
+                {
+                    DateFrom = new DateTime(2017, 2, 21, 19, 0, 0),
+                    DateTo = new DateTime(2017, 2, 21, 22, 0, 0),
+                    EventMadeBy = "Jen",
+                    IsActive = true,
+                    ActivityId = context.Activity.FirstOrDefault(c => c.ActivityDescription ==  "Rock Concert").ActivityId,
+                    CreationDate = new DateTime(2017, 2, 12, 0, 0, 12)
+                },
+                new Event
+                {
+                    DateFrom = new DateTime(2017, 2, 18, 13, 0, 0),
+                    DateTo = new DateTime(2017, 2, 18, 17, 0, 0),
+                    EventMadeBy = "a",
+                    IsActive = true,
+                    ActivityId = context.Activity.FirstOrDefault(c => c.ActivityDescription ==  "Golf Tournament").ActivityId,
+                    CreationDate = new DateTime(2017, 2, 12, 0, 0, 13)
+                },
+                new Event
+                {
+                    DateFrom = new DateTime(2017, 2, 15, 9, 0, 0),
+                    DateTo = new DateTime(2017, 2, 15, 10, 0, 0),
+                    EventMadeBy = "a",
+                    IsActive = false,
+                    ActivityId = context.Activity.FirstOrDefault(c => c.ActivityDescription ==  "Swimming Race").ActivityId,
+                    CreationDate = new DateTime(2017, 2, 12, 0, 0, 14)
+                },
+                new Event
+                {
+                    DateFrom = new DateTime(2017, 2, 16, 12, 0, 0),
+                    DateTo = new DateTime(2017, 2, 16, 13, 0, 0),
+                    EventMadeBy = "a",
+                    IsActive = true,
+                    ActivityId = context.Activity.FirstOrDefault(c => c.ActivityDescription ==  "Doubles Tennis Game").ActivityId,
+                    CreationDate = new DateTime(2017, 2, 12, 0, 0, 15)
+                },
+                new Event
+                {
+                    DateFrom = new DateTime(2017, 2, 15, 16, 0, 0),
+                    DateTo = new DateTime(2017, 2, 15, 18, 0, 0),
+                    EventMadeBy = "a",
+                    IsActive = true,
+                    ActivityId = context.Activity.FirstOrDefault(c => c.ActivityDescription ==  "Family Bowling").ActivityId,
+                    CreationDate = new DateTime(2017, 2, 12, 0, 0, 16)
                 }
             };
 
